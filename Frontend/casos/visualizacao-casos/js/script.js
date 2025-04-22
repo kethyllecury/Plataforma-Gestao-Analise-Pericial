37a34853c7f5418f187b00e0865c9b2a1f6bc1eb
Gabriel Meikii <gabrielmaeda01@gmail.com>
2025-04-22 12:43:12 -0300

    feat: Conexão teste API com get casos

---
+++ Frontend/casos/visualizacao-casos/js/script.js
@@ -0,0 +1,31 @@
+async function retornarCasos() {
+    const url = "http://localhost:3000/casos";
+    const tabela = document.querySelector('#tabela-corpo');
+    
+    try {
+        const requisicao = await fetch(url);
+        const data = await requisicao.json();
+        
+        // Mapeia os casos para criar um array de strings HTML e junta com join('')
+        tabela.innerHTML = data.map(caso => templateCaso(caso)).join('');
+    } catch (erro) {
+        alert(`Erro na requisição: ${erro.message}`);
+    }
+}
+
+function templateCaso(caso) {
+    return `<tr>
+                <td class="titulo-caso">${caso.nome}</td>
+                <td class="descricao-caso">${caso.descricao}</td>
+                <td class="tipo-caso">${caso.tipo}</td>
+                <td class="status-caso"><span class="badge status-${caso.status.toLowerCase().replace(" ", "-")}">${caso.status}</span></td>
+                <td class="data-abertura-caso">${caso.data}</td>
+                <td class="perito-caso">${caso.peritoResponsavel}</td>
+                <td><a href="../visualizacao-caso-especifico/visualizacao-de-caso.html" class="btn btn-acessar-caso btn-sm">Acessar caso</a></td>
+            </tr>`;
+}
+
+// Executa a função após o DOM estar carregado
+document.addEventListener('DOMContentLoaded', () => {
+    retornarCasos();
+});
